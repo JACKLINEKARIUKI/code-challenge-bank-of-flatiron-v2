b12a043643083e6985e8dd82fcde0c5d97980d3b
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-09-30 09:22:09 +0300

    Completed assignment

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((r) => r.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+  const transactionsList = useMemo(
+    () =>
+      transactions.filter((transaction) =>
+        transaction.description.toLowerCase().includes(search.toLowerCase())
+      ),
+    [transactions, search]
+  );
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search filter={setSearch} />
+      <AddTransactionForm
+        addTransaction={setTransactions}
+        transactions={transactions}
+      />
+      <TransactionsList transactions={transactionsList} />
     </div>
   );
 }
